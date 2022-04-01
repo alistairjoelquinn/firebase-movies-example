ba8b86d3d9531836818b69aee5c78a3ff8fc77ee
Alistair Quinn <alistairjoelquinn@gmail.com>
2022-04-01 16:23:43 +0200

    adds firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, get, child } from 'firebase/database';
+import { useEffect, useState } from 'react';
+import firebaseConfig from '../lib/firebase';
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 const Home: NextPage = () => {
-    console.log('home page');
+    const [movie, setMovie] = useState('');
+    const [rating, setRating] = useState('');
+
+    const submitHandler = () => set(ref(db, `movies/${movie}`), { rating });
+
+    useEffect(() => {
+        get(child(ref(db), `movies`)).then(response =>
+            response.exists() ? console.log(response.val()) : console.log('error'),
+        );
+    });
+
     return (
         <>
             <Head>
@@ -10,7 +28,21 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.png" />
             </Head>
             <div className="flex min-h-screen flex-col items-center justify-center py-2">
-                <h1 className="text-4xl text-red-500">I am the HOME PAGE</h1>
+                <input
+                    className="border-2"
+                    type="text"
+                    value={movie}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMovie(e.target.value)}
+                />
+                <input
+                    className="border-2"
+                    type="text"
+                    value={rating}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setRating(e.target.value)}
+                />
+                <button type="button" onClick={submitHandler}>
+                    Submit
+                </button>
             </div>
         </>
     );
